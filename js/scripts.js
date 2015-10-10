c93b05d48fc8bf7d393f99f29196228417f375ad
Katie O'Leary <olearyk17@gmail.com>
2015-10-09 22:50:42 -0400

    finishing up cheat sheet

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,50 @@
-//variable
+//Variable
 var x=8
 
-//debugging
+//Debugging
 //this is a comment
+/*This comment is 
+two lines*/
 console.log("Testing 1, 2 ,3");
 alert("Oops! Something went wrong!")
+
+//Data Types
+//numbers
+var distance = 4;
+//string
+var nickName = "Katie";
+//boolean
+var x = true;
+//arrays
+var dogBreeds = ["Sheltie", "Samoyed", "Pomeranian", "Corgi"];
+
+//Testing
+console.log("Helps us test things in the browsers console to see if we get the outcome we expect");
+
+//Logic
+if(10<3){
+	console.log("This is true");
+}else{
+	console.log("This is false");
+}
+
+
+//Functions
+
+function myFunction() {
+    document.getElementById("test").innerHTML = "Thanks for clicking!";
+}
+
+
+
+if(8<4){
+    console.log("I suck at javascript");
+}else{
+    console.log("I'm the best!");
+}
+
+
+
+var myName = "Katie";
+
+console.log(myName.length);
